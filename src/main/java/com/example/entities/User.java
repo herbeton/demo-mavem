506c73bd83f9d89d1e18b19e744f92beb2860992
Herbeton <herbeton7@gmail.com>
2023-03-22 19:23:42 -0300

    ADD a entidade User.

---
+++ src/main/java/com/example/entities/User.java
@@ -0,0 +1,45 @@
+package com.example.entities;
+
+public class User {
+
+    private Long Id;
+    private String name;
+    private String email;
+    private Department department;
+
+    public User(){
+
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
