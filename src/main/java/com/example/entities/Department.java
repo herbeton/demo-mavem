a979a097906f3712e869324fc7e904c7b555ef5e
Herbeton <herbeton7@gmail.com>
2023-03-22 19:17:02 -0300

    Add a entidade departamento.

---
+++ src/main/java/com/example/entities/Department.java
@@ -0,0 +1,26 @@
+package com.example.entities;
+
+public class Department {
+    private int Id;
+    private String name;
+
+    public Department(){
+
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
