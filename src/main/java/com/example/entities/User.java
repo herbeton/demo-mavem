1cc6ff819294aa3147008e06763cb04c0d462eb1
Herbeton <herbeton7@gmail.com>
2023-03-24 19:49:12 -0300

    MOD a importação das entidades User e Department.

--- src/main/java/com/example/entities/User.java
+++ src/main/java/com/example/entities/User.java
@@ -1,11 +1,17 @@
 package com.example.entities;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
 
 @Entity
 @Table(name = "tb_user")
 public class User {
-    @javax.persistence.Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     private String name;
