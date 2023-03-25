14e34b0cd49423a3360e97c82f3742402ae7441c
Herbeton <herbeton7@gmail.com>
2023-03-25 10:13:15 -0300

    MOD no Controller do User para usar a implementação do Service e não ter a implementação direto controller.

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,9 +1,8 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.User;
-import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,21 +10,25 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @Autowired
-    private UserRepository repository;
+    private UserService userService;
     @GetMapping
     public List<User> findAll(){
-        List<User> result = repository.findAll();
+        List<User> result = userService.findAll();
         return result;
     }
     @GetMapping(value = "/{id}")
     public User findById(@PathVariable Long id){
-        User result = repository.findById(id).get();
+        User result = userService.findById(id);
         return result;
     }
     @PostMapping
     public User insert(@RequestBody User user){
-        User result = repository.save(user);
+        User result = userService.insert(user);
         return result;
     }
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,7 +1,12 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.User;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -9,5 +14,20 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(){
+        List<User> users = userRepository.findAll();
+        return users;
+    }
+
+    public User findById(@PathVariable Long id){
+        User user = userRepository.findById(id).get();
+        return user;
+    }
+
+    public User insert(@RequestBody User user){
+        User userInser = userRepository.save(user);
+        return userInser;
     }
 }
