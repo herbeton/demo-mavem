23c04f9f3f16ab381a38fd20e819d8c717fab373
Herbeton <herbeton7@gmail.com>
2023-03-25 10:03:51 -0300

    ADD o service do User.

---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,13 @@
+package com.example.demo.services;
+
+import com.example.demo.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
